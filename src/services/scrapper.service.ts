bd7387248064053ddddbc9dbf64590b5ce9781ae
jonathan VARELA <jonathan.varela@zenika.com>
2020-06-30 11:20:18 +0200

    remove useless method

--- src/services/scrapper.service.ts
+++ src/services/scrapper.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ScrapperService {
 
-  getHello(): string {
-    return 'Hello World!';
-  }
-
   getManga(mangaUrlBase64: string) {
     return Promise.resolve(undefined);
   }
