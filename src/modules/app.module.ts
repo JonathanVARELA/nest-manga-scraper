1b4c38cd0f3b71610536d2b53f52c79f3ad7852b
jonathan VARELA <jonathan.varela@zenika.com>
2020-06-30 17:49:03 +0200

    remove app.service.ts

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/app.service';
 import { MangaController } from '../controllers/manga.controller';
 import { ScrapperService } from '../services/scrapper.service';
 import { CsvService } from '../services/csv.service';
@@ -10,6 +9,6 @@ import { MangaService } from '../services/manga.service';
 @Module({
   imports: [LoggerModule],
   controllers: [AppController, MangaController],
-  providers: [AppService, ScrapperService, CsvService, MangaService],
+  providers: [ScrapperService, CsvService, MangaService],
 })
 export class AppModule {}
