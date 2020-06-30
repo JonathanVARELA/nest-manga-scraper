bbcc6a3abd3ae414e9c1d8df4daa8fd9e948521f
jonathan VARELA <jonathan.varela@zenika.com>
2020-06-30 19:16:24 +0200

    setup cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ if (process.env.MODE !== 'prod') {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  await app.listen(5000);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.WHITE_LIST);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  await app
+    .listen(5000);
 }
 bootstrap();
